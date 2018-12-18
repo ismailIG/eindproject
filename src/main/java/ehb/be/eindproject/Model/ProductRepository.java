61644b6747c092eaf5fb722e986d7c23a2479cb0
ismailIG <ismail6s@yahoo.com>
2018-12-18 15:22:25 +0100

    clean up products merge conflicts index solved

--- src/main/java/ehb/be/eindproject/Model/ProductRepository.java
+++ src/main/java/ehb/be/eindproject/Model/ProductRepository.java
@@ -2,5 +2,11 @@ package ehb.be.eindproject.Model;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+    List<Product> findByDier(String welkDier);
+    List<Product> findByCategorie(String welkeCategorie);
+
 }
