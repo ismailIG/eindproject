1523cf46ee0712a3f93a6f586d4a8cce4be84dc0
ismailIG <ismail6s@yahoo.com>
2018-12-18 17:08:41 +0100

    local changes index

--- src/main/java/ehb/be/eindproject/Controller/IndexController.java
+++ src/main/java/ehb/be/eindproject/Controller/IndexController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 @Controller
-public class indexController {
+public class IndexController {
     @Autowired
     ProductRepository repo;
     @ModelAttribute("all")
