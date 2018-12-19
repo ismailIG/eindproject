0de2194c893b9693d2957f8856bc647485114a08
fula85 <carine_fulakula@hotmail.com>
2018-12-19 12:16:00 +0100

    winkelmand html gemaakt en CSS main bijgevoegd Kattencontroller verandert naar DetailsController

--- src/main/java/ehb/be/eindproject/Model/Winkelmand.java
+++ src/main/java/ehb/be/eindproject/Model/Winkelmand.java
@@ -4,8 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.HashSet;
 
 
 public class Winkelmand {
-
+HashSet<String> hSet = new HashSet<>();
 }
